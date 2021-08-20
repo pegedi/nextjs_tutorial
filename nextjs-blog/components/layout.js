cea5bdbccd4d7275841f7ceb2d51aba185e9fbf1
Peter Egedi <peter.egedi@gmail.com>
2021-08-20 20:46:49 +0000

    GetStaticPath for dynamic pages

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Peter Zsolt'
+const name = 'Peter Zsolt :)'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
